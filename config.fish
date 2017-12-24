a0564e831d2e28f1ba5f41a87f2b376e72f37b41
Roger González <rogergonzalez21@gmail.com>
2017-12-23 20:47:34 -0400

    Latest iteration

--- config.fish
+++ config.fish
@@ -1,1 +1,5 @@
 set fish_greeting ""
+
+set -g -x ANDROID_HOME ~/Android/Sdk
+set -gx PATH /home/roger/Android/Sdk/tools /home/roger/Android/Sdk/platform-tools $PATH
+set -gx JAVA_HOME /usr/lib/jvm/java-8-jdk
